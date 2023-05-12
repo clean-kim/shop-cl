44fb2c955f5965c91182b3287d8281e17993744b
cleankim <112611552+cleankim@users.noreply.github.com>
2023-05-12 17:00:43 +0900

    장바구니 퍼블리싱 진행중

--- src/components/cart/CartMobile.tsx
+++ src/components/cart/CartMobile.tsx
@@ -3,15 +3,24 @@ import {CartData} from "../../interface/Product";
 import img from "../../assets/img/img.png";
 
 export default function CartMobile(params: CartData) {
+    const calculator = (sign: number) => {
+
+    }
 
     return (
         <CartMobileBox>
             <ProductBox>
-                <Checkbox htmlFor="checkbox">
-                    <input type="checkbox" id="checkbox"/>
-                    <span></span>
+                <Checkbox>
+                    <input type="checkbox" id="icb" onInput={e => {console.log(e)}}/>
+                    <label htmlFor="icb" />
                 </Checkbox>
-                <img src={img} alt="상품 이미지"/>
+                <ProductImg src={img} alt="상품 이미지"/>
+                <InfoBox>
+                    <BrandName>brand name</BrandName>
+                    <Title>title title title</Title>
+                    <Count>1</Count>
+                    <Price>price</Price>
+                </InfoBox>
             </ProductBox>
         </CartMobileBox>
     );
@@ -34,29 +43,74 @@ const ProductBox = styled.div`
   }
 `;
 
-const Checkbox = styled.label`
-  position: relative;
-  width: 18px;
-  margin-right: 8px;
-  user-select: none;
+const ProductImg = styled.img`
+  border-radius: 7px;
+`;
+
+const Checkbox = styled.span`
+  display: block;
+  margin-right: 12px;
   
-  &:after {
-    content: '';
-    position: absolute;
-    display: none;
-  }
-  
-  input[type=checkbox] {
-    appearance: none;
-  }
-  
-  span {
-    position: absolute;
-    top: 0;
-    left: 0;
+  input[type='checkbox']+label {
+    display: block;
+    position: relative;
     width: 18px;
     height: 18px;
-    border: 1xp solid var(--border200);
-    border-radius: 100%;
   }
+
+  input[type='checkbox']+label::before {
+    content: '';
+    position: absolute;
+    width: 18px;
+    height: 18px;
+    top: 0;
+    left: 0;
+    display: block;
+    background-color: var(--ui-background);
+    border: 2px solid var(--border300);
+    border-radius: 50%;
+  }
+
+  input[type='checkbox'] {
+    position: absolute;
+    opacity: 0;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    width: 18px;
+    height: 18px;
+  }
+  
+  input[type='checkbox']:checked+label:before{
+    position: absolute;
+    content: '';
+    background: url(${`/img/checked.svg`}) center no-repeat;
+    background-size: 18px;
+    background-color: var(--primary);
+    border: 2px solid var(--primary);
+    border-radius: 50%;
+  }
+`;
+
+const InfoBox = styled.div`
+  margin-left: 10px;
+`;
+
+const BrandName = styled.div`
+  font-size: 13px;
+`;
+
+const Title = styled.div`
+  font-size: 14px;
+  word-break: break-all;
+  text-overflow: ellipsis;
+`;
+
+const Price = styled.div`
+  font-size: 14px;
+  font-weight: 700;
+`;
+
+const Count = styled.div`
+  font-size: 12px;
 `;
