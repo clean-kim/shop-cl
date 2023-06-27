fd49acc950e4112f47c40034011d428d7306b611
cleankim <kimlin8798@naver.com>
2023-06-27 19:38:40 +0900

    test

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 import {useLocation} from "react-router";
-import { Swiper, SwiperSlide } from 'swiper/react';
 import "swiper/css";
 import "swiper/css/navigation";
 import 'swiper/swiper.min.css';
-import {Link} from 'react-router-dom';
 import {PC, M} from "@components/common/MediaQuery";
 import {useEffect, useState} from 'react';
 import NavPC from '@components/common/NavPC';
